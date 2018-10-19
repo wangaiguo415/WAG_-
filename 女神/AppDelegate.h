ca82b2cd8fcadf455c6abacd7d67de173e8151c7
王爱国 <wangag0308@wisea.com.cn>
2018-10-19 11:02:49 +0800

    Initial Commit

---
+++ 女神/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  女神
+//
+//  Created by wangaiguo on 2018/10/19.
+//  Copyright © 2018年 王爱国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 女神/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  女神
+//
+//  Created by wangaiguo on 2018/10/19.
+//  Copyright © 2018年 王爱国. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
