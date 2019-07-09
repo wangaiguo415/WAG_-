897b3bd56636d9aaddc9177179e3267ab0697869
王爱国 <wangag0308@wisea.com.cn>
2019-07-09 17:50:33 +0800

    add getLanuchImage

---
+++ 女神/utils/获取启动图片/WAGGetLaunchImage.h
@@ -0,0 +1,17 @@
+//
+//  WAGGetLaunchImage.h
+//  女神
+//
+//  Created by wangaiguo on 2019/7/9.
+//  Copyright © 2019 王爱国. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WAGGetLaunchImage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 女神/utils/获取启动图片/WAGGetLaunchImage.m
@@ -0,0 +1,13 @@
+//
+//  WAGGetLaunchImage.m
+//  女神
+//
+//  Created by wangaiguo on 2019/7/9.
+//  Copyright © 2019 王爱国. All rights reserved.
+//
+
+#import "WAGGetLaunchImage.h"
+
+@implementation WAGGetLaunchImage
+
+@end
